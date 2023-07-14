add type hints clean-up and correct unit descriptions add test function for epsilon computation

model/compute_epsilon.py
@@ -12,11 +12,11 @@ from scipy.optimize import minimize
 from model.fcn_MNC import fnc_M, fnc_N, fnc_Cm
 
 
-def residual_of_mass_flow_deficits(epsilon, d0, Ct, r0):
+def residual_of_mass_flow_deficits(epsilon: float, d0: float, Ct: float, r0: float) -> float:
     """
     Computes the residual between the mass flow deficits from gaussian wake and Frandsen wake.
 
-    :param epsilon: wake expansion at x0 [m]
+    :param epsilon: wake expansion at x0 [D]
     :param d0: rotor diameter [m]
     :param Ct: thrust coefficient [-]
     :param r0: span-wise location of the Gaussian extrema [m]
@@ -30,18 +30,18 @@ def residual_of_mass_flow_deficits(epsilon, d0, Ct, r0):
     mDot_dg = np.pi * M * Cm  # ignoring air density
     beta = (1 / 2) * ((1 + np.sqrt(1 - Ct)) / (np.sqrt(1 - Ct)))
     mDot_frandsen = (np.pi / 8) * (d0 ** 2) * beta * (1 - np.sqrt(1 - ((2 / beta) * Ct)))  # ignoring air density
+    residual = (mDot_dg - mDot_frandsen) ** 2  # ignoring air density
+    return residual
 
-    return (mDot_dg - mDot_frandsen) ** 2
 
-
-def compute_epsilon(d0, Ct, r0):
+def compute_epsilon(d0: float, Ct: float, r0: float) -> float:
     """
     Computes epsilon as function of diameter, thrust coefficent and spanwise location of the Gaussian extrema.
 
     :param d0: rotor diameter [m]
     :param Ct: thrust coefficient [-]
     :param r0: span-wise location of the Gaussian extrema [m]
-    :return epsilon: wake expansion at x0 [m]
+    :return epsilon: wake expansion at x0 [D]
     """
 
     with warnings.catch_warnings():

model/double_gaussian.py
@@ -12,9 +12,9 @@ from model.compute_epsilon import compute_epsilon
 from model.fcn_MNC import fnc_M, fnc_N, fnc_Cm
 
 
-def double_gaussian_deficit(x: np.ndarray, y: np.ndarray, z: np.ndarray, Ct: float, d0: float,
-                            kr: float, x0: float, k: float,
-                            recompute_epsilon: bool, fcn_epsilon: callable = None):
+def double_gaussian_deficit(x: np.ndarray, y: np.ndarray, z: np.ndarray,
+                            Ct: float, d0: float, kr: float, x0: float, k: float,
+                            recompute_epsilon: bool, fcn_epsilon: callable = None) -> np.ndarray:
     """
        Computes the double gaussian wake deficit at given locations.
 
@@ -23,30 +23,26 @@ def double_gaussian_deficit(x: np.ndarray, y: np.ndarray, z: np.ndarray, Ct: flo
        :param z: downstream z-locations to be evaluated [m]
        :param Ct: thrust coefficient [-]
        :param d0: rotor diameter [m]
-       :param kr: wake parameter, position of the Gaussian extrema [D] (0: wake center, 1: blade tip)
+       :param kr: wake parameter, position of the Gaussian extrema (0: wake center, 1: blade tip) [-]
        :param x0: wake parameter, position stream tube outlet [D]
        :param k: wake parameter, slope of wake expansion [-]
        :param recompute_epsilon: flag, whether epsilon shall be computed on-the-fly (true) or whether a pre-defined function shall be evaluated (false)
        :param fcn_epsilon: pre-computed epsilon function with parameters Ct [-] and kr [D], only used if recompute_epsilon=False
-       :return deficit: wake deficit at each location [-]
+       :return deficit: wake deficit at each location x, y, z (wake velocity = free-stream velocity v * (1 - deficit) [-]
        """
 
-    # todo: is "[D]" correct? In "Table 1" we claimed it is "[-]"
-    # "r0 is radial position of gaussian extrema" - this is in SI units (meters)
-    # but: "When kr=1 the curve extrema are located at the tip of the rotor blades, while for kr=0
-    # the two Gaussian functions coincide at the wake center"
-    # but: "r0=kr/2, or kr=2*r0" -> shouldn't this be "kr = r0/(0.5*d), or r0=kr/2 * D" (DIAMETER MISSING IN EQN in 3.2)]
-
-    # r0, span-wise location of the Gaussian extrema [m]
-    r0 = kr * d0 / 2.0  # [m]
+    # r0, span-wise location of the Gaussian extrema
+    r0_D = kr / 2  # [D]
+    r0 = r0_D * d0  # [m]
 
     if recompute_epsilon:
         # solve epsilon function on-the-fly
-        epsilon = compute_epsilon(d0, Ct, r0)
+        epsilon = compute_epsilon(d0, Ct, r0)  # [D]
     else:
         # use precomputed epsilon function
         epsilon = fcn_epsilon((Ct, kr))  # [D]
 
+    # sig, wake expansion
     sig = k * (x - x0 * d0) + epsilon * d0  # [m]
 
     M = fnc_M(sig, r0)
@@ -60,7 +56,5 @@ def double_gaussian_deficit(x: np.ndarray, y: np.ndarray, z: np.ndarray, Ct: flo
 
     if np.isnan(deficit).sum() > 0:
         warnings.warn("Deficit could not be computed in every location!")
-        # warnings.warn("Deficit could not be computed in every location! Setting respective deficits to 0.")
-        # deficit[np.isnan(deficit)] = 0.0
 
     return deficit

model/fcn_MNC.py
@@ -8,7 +8,7 @@ import numpy as np
 from scipy import special
 
 
-def fnc_M(sig, r0):
+def fnc_M(sig: float, r0: float) -> float:
     """
     Computes 'M' according to Eqn. 7a
 
@@ -20,7 +20,7 @@ def fnc_M(sig, r0):
         np.sqrt(2 * np.pi) * r0 * sig * special.erf(r0 / (sig * np.sqrt(2)))
 
 
-def fnc_N(sig, r0):
+def fnc_N(sig: float, r0: float) -> float:
     """
     Computes 'N' according to Eqn. 7b
 
@@ -32,17 +32,15 @@ def fnc_N(sig, r0):
         (1 / 2) * np.sqrt(np.pi) * r0 * sig * special.erf(r0 / sig)
 
 
-def fnc_Cm(M, N, Ct, d0):
+def fnc_Cm(M: float, N: float, Ct: float, d0: float) -> float:
     """
-    Computes 'Cm' according to Eqn. 8 (only C-minus, Cm, is computed)
+    Computes amplitude 'Cm' according to Eqn. 8 (only C-minus, Cm, is computed)
 
     :param M: M [m^2]
     :param N: N [m^2]
     :param Ct: thrust coefficient [-]
     :param d0: rotor diameter [m]
-    :return Cm [-]
+    :return Cm: amplitude [-]
     """
     below_sqrt = (M ** 2.0) - 0.5 * N * Ct * (d0 ** 2)
     return (M - np.sqrt(below_sqrt)) / (2.0 * N)
-
-