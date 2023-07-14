222553cfcfb215c64080558514fa10b2a211834c
gogannes <gogannes@gmail.com>
2023-07-14 10:16:43 +0200

    add license (BSD License, 2-clause) see: https://gist.github.com/nicolasdao/a7adda51f2f185e8d2700e1573d8a633

--- tests/test_double_gaussian.py
+++ tests/test_double_gaussian.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2023, Johannes Schreiber; Amr Balbaa; Carlo L. Bottasso
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 import pickle
 
 import numpy as np
